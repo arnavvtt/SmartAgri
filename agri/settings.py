5b009c7e8e85e7c2dca52aff611bed7bb81cb799
arnavvtt <tyagiarnav24@gmail.com>
2026-05-13 13:52:35 +0530

    fix allowed hosts

--- agri/settings.py
+++ agri/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-*7hwxj_s8q4#l_m=7yp!z5j2yo$-y)z2y6w+bfl_&0y&fzm$5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["smartagris.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
